37728eb7ed1b5d12d9144171177e239c226969d0
Gaf-ker <61698492+Gaf-ker@users.noreply.github.com>
2022-08-06 13:10:01 +0300

    Add files via upload

---
+++ simple_noise.py
@@ -0,0 +1,17 @@
+from PIL import Image
+from random import randint
+import numpy as np
+WIDTH = 1000
+HEIGHT = 1000
+
+a = []
+
+for i in range(HEIGHT):
+    b = [randint(0, 255) for j in range(WIDTH)]
+    a.append(b)
+
+a = np.array(a, dtype=np.uint8)
+
+img = Image.fromarray(a)
+img.save('simple.png')
+img.show()
